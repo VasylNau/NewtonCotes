d4d5a40d06f94801c485ee4d8439a8bbf225bfa3
VasylNau <vasy1nauly4nyi@gmail.com>
2018-03-22 14:03:00 +0200

    Init commit. Added base project structure

---
+++ src/main/java/edu/lpnu/cad/newtoncotes/NewtonCotesWebApp.java
@@ -0,0 +1,20 @@
+package edu.lpnu.cad.newtoncotes;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class NewtonCotesWebApp {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(NewtonCotesWebApp.class, args);
+	}
+
+}
